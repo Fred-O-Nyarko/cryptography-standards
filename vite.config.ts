b1db5390e8044f275f93cd8d1eb13fdbcb180963
Frederick Obeng-Nyarko <nernuer@gmail.com>
2026-06-11 20:36:25 +0000

    Add .netlify folder to .gitignore; create netlify.toml for build and dev commands; remove package-lock.json; update package.json and pnpm-lock.yaml for new dependencies; modify Vite configuration to include Netlify plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import netlifyReactRouter from "@netlify/vite-plugin-react-router";
+import netlify from "@netlify/vite-plugin";
+
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), netlifyReactRouter()],
+  plugins: [tailwindcss(), reactRouter(), netlifyReactRouter(), netlify()],
   resolve: {
     tsconfigPaths: true,
   },
